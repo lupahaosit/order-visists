ae084353d136b6cbb67e19d5ff4d70fc4e566e29
lupahaosit <igor2002vlad@mail.ru>
2025-05-25 03:01:59 +0500

    start

--- app/src/main/java/com/example/schoolmarksproject/MainActivity.kt
+++ app/src/main/java/com/example/schoolmarksproject/MainActivity.kt
@@ -1866,6 +1866,7 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                     }
+                    
                 }
             }
         }
